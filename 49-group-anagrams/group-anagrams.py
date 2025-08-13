407a1ae5082c52571f34e76c009c0fde04b64d7e
Manjusree Pushphaa <128013690+TechSorceressree@users.noreply.github.com>
2025-08-13 18:45:12 -0500

    Time: 12 ms (59.74%) | Memory: 20.6 MB (90.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = defaultdict(list) #Automatically assigns a default value to any key that does not exist in the dictionary
-
+        result = defaultdict(list)
+        
         for s in strs:
-            count = [0] * 26
-
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-            result[tuple(count)].append(s)          #Using immutable alternatives to represent list-like data as keys as Lists cannot be dictionary keys because they are mutable and not hashable
-
-        return list(result.values())    #return list of the values of the dictionar E.g: ["eat","tea","tan","ate","nat","bat"]. Also, the function (-> List[List[str]]) expects Lists of list of strings to be returned 
+            key = "".join(sorted(s))
+            result[key].append(s)
+        return list(result.values())
