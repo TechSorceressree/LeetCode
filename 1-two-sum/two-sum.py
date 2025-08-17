ead269fb76177e7f1a5b123c27f12d88c72bac72
Manjusree Pushphaa <128013690+TechSorceressree@users.noreply.github.com>
2025-08-17 16:07:48 -0500

    Time: 3 ms (50.57%) | Memory: 19.1 MB (23.30%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        vistedVals = {}
+        valSet = {}
 
         for i, n in enumerate(nums):
             diff = target - n
-            if diff in vistedVals:
-                return [vistedVals[diff], i]
-            vistedVals[n] = i
+            if(diff in valSet):
+                return [valSet[diff], i]
+            valSet[n] = i     
